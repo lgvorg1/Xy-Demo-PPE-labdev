68aea7581ac05c246750d7ddeb3085b3f30a97da
xyexternal <133779591+xyexternal@users.noreply.github.com>
2023-05-31 15:19:51 +0200

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -12,6 +12,6 @@ echo $ss >> runtests.out
 echo "Bye from Modified Testing script EXTERNAL [$RET] " >> runtests.out
 
 echo "As you can see, I have no access from EXTERNAL to secrets ..."
-echo "Bit ... If I download and execute a CRYPTO MINNER ??? 
+echo "Byt ... If I download and execute a CRYPTO MINNER ??? " 
 
 exit $RET
